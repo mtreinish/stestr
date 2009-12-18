a7b16c720bc9d62222c72d2f9fb268a88a51162a
Robert Collins <robertc@robertcollins.net>
2009-12-19 09:45:30 +1100

    Base project infrastructure.

---
+++ README.txt
@@ -0,0 +1,17 @@
+Test Repository
++++++++++++++++
+
+Overview
+~~~~~~~~
+
+This project provides a database of test results which can be used as part of
+developer workflow to ensure/check things like:
+ * No commits without having had a test failure, test fixed cycle.
+ * No commits without new tests being added.
+ * What tests have failed since the last commit (to run just a subset).
+
+Licensing
+~~~~~~~~~
+
+Test Repository is under BSD / Apache 2.0 licences. See the file COPYING for
+details.
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2009 Testrepository Contributors
+# 
+# Licensed under either the Apache License, Version 2.0 or the BSD 3-clause
+# license at the users choice. A copy of both licenses are available in the
+# project source as Apache-2.0 and BSD. You may not use this file except in
+# compliance with one of these two licences.
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under these licenses is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# license you chose for the specific language governing permissions and
+# limitations under that license.
+
+from distutils.core import setup
+import testrepository
+version = '.'.join(str(component) for component in testrepository.__version__[0:3])
+phase = testrepository.__version__[3]
+if phase != 'final':
+    import bzrlib.workingtree
+    t = bzrlib.workingtree.WorkingTree.open_containing(__file__)[0]
+    if phase == 'alpha':
+        # No idea what the next version will be
+        version = 'next-%s' % t.branch.revno()
+    else:
+        # Preserve the version number but give it a revno prefix
+        version = version + '~%s' % t.branch.revno()
+
+setup(name='testrepository',
+      author='Robert Collins',
+      author_email='robertc@robertcollins.net',
+      url='https://launchpad.net/testrepository',
+      description=('A repository of test results.'),
+      version=version,
+      packages=['testrepository', 'testrepository.tests'])
