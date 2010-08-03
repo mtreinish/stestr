3d8db38e1b4d3e7ec82a5ca659ae724ad12dbb51
Jonathan Lange <jml@canonical.com>
2010-08-03 19:50:08 +0100

    Remove unnecessary lines.

--- testrepository/ui/cli.py
+++ testrepository/ui/cli.py
@@ -131,7 +131,6 @@ class UI(ui.AbstractUI):
         self._stdout.write('%s\n' % ' '.join(outputs))
 
     def _check_cmd(self):
-        cmd = self.cmd
         parser = OptionParser()
         parser.add_option("-d", "--here", dest="here",
             help="Set the directory or url that a command should run from. "
@@ -145,7 +144,6 @@ class UI(ui.AbstractUI):
         options, args = parser.parse_args(self._argv)
         self.here = options.here
         self.options = options
-        orig_args = list(args)
         parsed_args = {}
         failed = False
         for arg in self.cmd.args:
