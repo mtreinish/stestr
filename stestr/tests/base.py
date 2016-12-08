ec2cee843d8c187b9a724fa6b2030001c1f6d8bd
Matthew Treinish <mtreinish@kortar.org>
2016-12-08 16:48:58 -0500

    Nuke the testrepository unit tests
    There is no value in keeping the testr unit tests around. They depend
    heavily on the previous structure of the repo and won't ever work after
    the significant changes made already. It'll just be easier to craft new
    tests from scratch. If there are any pieces form the old testr unit
    tests that will be useful for the new tests we can always just manually
    copy them back in.

---
+++ stestr/tests/base.py
@@ -0,0 +1,33 @@
+# Licensed under either the Apache License, Version 2.0 or the BSD 3-clause
+# license at the users choice. A copy of both licenses are available in the
+# project source as Apache-2.0 and BSD. You may not use this file except in
+# compliance with one of these two licences.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under these licenses is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# license you chose for the specific language governing permissions and
+# limitations under that license.
+
+import os
+
+import fixtures
+import testtools
+
+
+class TestCase(testtools.TestCase):
+
+    true = ('True', 'true', '1', 'yes')
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        if os.environ.get('OS_STDOUT_CAPTURE') in self.true:
+            stdout = self.useFixture(fixtures.StringStream('stdout')).stream
+            self.useFixture(fixtures.MonkeyPatch('sys.stdout', stdout))
+        if os.environ.get('OS_STDERR_CAPTURE') in self.true:
+            stderr = self.useFixture(fixtures.StringStream('stderr')).stream
+            self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
+        if (os.environ.get('OS_LOG_CAPTURE') != 'False' and
+            os.environ.get('OS_LOG_CAPTURE') != '0'):
+            self.useFixture(fixtures.LoggerFixture(nuke_handlers=False,
+                                                   level=None))
