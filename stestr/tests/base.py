a7534be2fdc147321f180aee38c8d5879bd3f4ad
Matthew Treinish <mtreinish@kortar.org>
2017-01-03 01:41:55 -0500

    Make unit test attachment fixtures always enabled
    The code used in the base test class was copied from other projects and
    made the stdout, stderr, and logging attachments enabled optionally by
    env variables. However there is no reason to do that since if there is
    any output it is best to capture it instead of dropping it. This commit
    removes the conditionals and always attaches these.

--- stestr/tests/base.py
+++ stestr/tests/base.py
@@ -10,25 +10,17 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import os
-
 import fixtures
 import testtools
 
 
 class TestCase(testtools.TestCase):
 
-    true = ('True', 'true', '1', 'yes')
-
     def setUp(self):
         super(TestCase, self).setUp()
-        if os.environ.get('OS_STDOUT_CAPTURE') in self.true:
-            stdout = self.useFixture(fixtures.StringStream('stdout')).stream
-            self.useFixture(fixtures.MonkeyPatch('sys.stdout', stdout))
-        if os.environ.get('OS_STDERR_CAPTURE') in self.true:
-            stderr = self.useFixture(fixtures.StringStream('stderr')).stream
-            self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
-        if (os.environ.get('OS_LOG_CAPTURE') != 'False' and
-            os.environ.get('OS_LOG_CAPTURE') != '0'):
-            self.useFixture(fixtures.LoggerFixture(nuke_handlers=False,
-                                                   level=None))
+        stdout = self.useFixture(fixtures.StringStream('stdout')).stream
+        self.useFixture(fixtures.MonkeyPatch('sys.stdout', stdout))
+        stderr = self.useFixture(fixtures.StringStream('stderr')).stream
+        self.useFixture(fixtures.MonkeyPatch('sys.stderr', stderr))
+        self.useFixture(fixtures.LoggerFixture(nuke_handlers=False,
+                                               level=None))
