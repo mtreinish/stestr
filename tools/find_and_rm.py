cd4942aa02da3e7d81b0272f603865ad14b492e1
Masayuki Igawa <masayuki.igawa@gmail.com>
2017-10-17 14:16:51 +0900

    Remove pyc files before running stestr in tox
    This commit add a removing *.pyc files command before running stestr in
    tox. Sometimes, old *.pyc files are remaining which is not good,
    basically. To do this, this patch adds a tiny python tool for Windows
    environments. Because `find` command in Windows doesn't equal to Linux
    one.

---
+++ tools/find_and_rm.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+
+def find_and_remove(suffix='.pyc'):
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            target = os.path.join(root, file)
+            if os.path.isfile(target) and target.endswith(suffix):
+                os.remove(target)
+
+
+if __name__ == '__main__':
+    find_and_remove()
